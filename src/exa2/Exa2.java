11512af95909534c0710b9135047fb0ea9fe0ba6
kevinrodriguez100 <167909724+lowfrax@users.noreply.github.com>
2024-07-19 05:33:51 -0600

    creacion3

--- src/exa2/Exa2.java
+++ src/exa2/Exa2.java
@@ -15,7 +15,7 @@ public class Exa2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here++
-        System.out.println("hola");
+        System.out.println("hola +");
     }
     
 }
