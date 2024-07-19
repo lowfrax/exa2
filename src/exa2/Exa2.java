0ec5e24a35437e20e23fb9a7e1f4e86ff2ce8c8b
DELL <DELL@Joseth>
2024-07-19 16:54:16 -0600

    Prueba

--- src/exa2/Exa2.java
+++ src/exa2/Exa2.java
@@ -16,6 +16,7 @@ public class Exa2 {
     public static void main(String[] args) {
         // TODO code application logic here++
         System.out.println("hola +++");
+        System.out.println("Hola a Todos");
     }
     
 }
